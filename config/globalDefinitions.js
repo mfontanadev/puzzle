b7caef79d57212d765c51e1ef878a10ccb00f0f1
mfontana <mfontanadev@gmail.com>
2017-06-24 17:27:08 -0300

    Try 3/n avoid scrolliong

--- config/globalDefinitions.js
+++ config/globalDefinitions.js
@@ -21,7 +21,7 @@ function MainLoopState()
 { 
 }
 
-Globals.C_APPLICATION_TITLE_AND_VERSION = 'Puzzle v1.0.1';
+Globals.C_APPLICATION_TITLE_AND_VERSION = 'Puzzle v1.0.2';
 function Globals() 
 { 
     this.m_mainLoopState = new MainLoopState(); 
